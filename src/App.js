3e7189fc84e632746caadf87bba604a2f8224ba0
fikrianggara <221810306@stis.ac.id>
2022-08-05 03:27:20 +0700

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing from "./pages/Landing";
 import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1>hello world1</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />}>
+          <Route index element={<Landing />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,188 @@
+import React, { Fragment } from "react";
+import { Popover, Transition } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/solid";
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+const Navbar = () => {
+  return (
+    <Popover className="bg-blue-900 p-2 w-full">
+      <div className="flex justify-between w-11/12 items-center mx-auto">
+        <a href="/">
+          <div className="font-bold text-white text-xl flex items-center justify-between space-x-4">
+            <img
+              className="w-12 aspect-square"
+              src="/bps-logo.png"
+              alt="logo bps"
+            />
+
+            <h1 className="text-xl italic">BADAN PUSAT STATISTIK</h1>
+          </div>
+        </a>
+        <ul className="font-medium text-white flex space-x-6 text-sm items-center">
+          <li id="beranda">Beranda</li>
+          <li id="senarai">Senarai Rencana Terbit</li>
+          <li id="produk-statistik">
+            <Popover className="relative">
+              {({ open }) => (
+                <>
+                  <Popover.Button
+                    // className={classNames(
+                    //   open ? "text-gray-900" : "text-gray-500",
+                    //   "group bg-white rounded-md inline-flex items-center font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    // )}
+                    className="group rounded-md inline-flex items-center font-medium focus:outline-none hover:bg-blue-900"
+                  >
+                    <span>Produk Statistik</span>
+                    <ChevronDownIcon
+                      className={classNames(
+                        open ? "rotate-180 " : "",
+                        "ml-2 h-5 w-5 duration-200 ease-in-out"
+                      )}
+                      aria-hidden="true"
+                    />
+                  </Popover.Button>
+
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-200"
+                    enterFrom="opacity-0 translate-y-1"
+                    enterTo="opacity-100 translate-y-0"
+                    leave="transition ease-in duration-150"
+                    leaveFrom="opacity-100 translate-y-0"
+                    leaveTo="opacity-0 translate-y-1"
+                  >
+                    <Popover.Panel className="absolute z-10 -ml-4 mt-3 transform px-2 min-w-[200px] sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2">
+                      <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
+                        <div className="relative grid gap-6 bg-white px-2 py-4 sm:gap-6 sm:p-4">
+                          <ul className="text-blue-400">
+                            <li className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
+                              fdsafdsafds
+                            </li>
+                            <li className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
+                              fdsafds
+                            </li>
+                            <li className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
+                              fdsafds
+                            </li>
+                          </ul>
+                        </div>
+                      </div>
+                    </Popover.Panel>
+                  </Transition>
+                </>
+              )}
+            </Popover>
+          </li>
+          <li>Layanan</li>
+          <li>
+            <Popover className="relative">
+              {({ open }) => (
+                <>
+                  <Popover.Button
+                    // className={classNames(
+                    //   open ? "text-gray-900" : "text-gray-500",
+                    //   "group bg-white rounded-md inline-flex items-center font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    // )}
+                    className="group rounded-md inline-flex items-center font-medium focus:outline-none hover:bg-blue-900"
+                  >
+                    <span>Tentang Kami</span>
+                    <ChevronDownIcon
+                      className={classNames(
+                        open ? "rotate-180 " : "",
+                        "ml-2 h-5 w-5 duration-200 ease-in-out"
+                      )}
+                      aria-hidden="true"
+                    />
+                  </Popover.Button>
+
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-200"
+                    enterFrom="opacity-0 translate-y-1"
+                    enterTo="opacity-100 translate-y-0"
+                    leave="transition ease-in duration-150"
+                    leaveFrom="opacity-100 translate-y-0"
+                    leaveTo="opacity-0 translate-y-1"
+                  >
+                    <Popover.Panel className="absolute z-10 -ml-4 mt-3 transform px-2 min-w-[200px] max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2">
+                      <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
+                        <div className="relative grid gap-6 bg-white px-2 py-4 sm:gap-6 sm:p-4">
+                          <ul className="text-blue-400">
+                            <li className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
+                              fdsafdsafds
+                            </li>
+                            <li className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
+                              fdsafds
+                            </li>
+                            <li className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
+                              fdsafds
+                            </li>
+                          </ul>
+                        </div>
+                      </div>
+                    </Popover.Panel>
+                  </Transition>
+                </>
+              )}
+            </Popover>
+          </li>
+          <li>PPID</li>
+          <li>
+            <Popover className="relative">
+              {({ open }) => (
+                <>
+                  <Popover.Button
+                    // className={classNames(
+                    //   open ? "text-gray-900" : "text-gray-500",
+                    //   "group bg-white rounded-md inline-flex items-center font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    // )}
+                    className="group rounded-md inline-flex items-center font-medium focus:outline-none hover:bg-blue-900"
+                  >
+                    <span>Bahasa</span>
+                    <ChevronDownIcon
+                      className={classNames(
+                        open ? "rotate-180 " : "",
+                        "ml-2 h-5 w-5 duration-200 ease-in-out"
+                      )}
+                      aria-hidden="true"
+                    />
+                  </Popover.Button>
+
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-200"
+                    enterFrom="opacity-0 translate-y-1"
+                    enterTo="opacity-100 translate-y-0"
+                    leave="transition ease-in duration-150"
+                    leaveFrom="opacity-100 translate-y-0"
+                    leaveTo="opacity-0 translate-y-1"
+                  >
+                    <Popover.Panel className="absolute z-10 -ml-4 mt-3 transform px-2 min-w-[200px] max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2">
+                      <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
+                        <div className="relative grid gap-6 bg-white px-2 py-4 sm:gap-6 sm:p-4">
+                          <ul className="text-blue-400">
+                            <li className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
+                              Indonesia
+                            </li>
+                            <li className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
+                              Inggris
+                            </li>
+                          </ul>
+                        </div>
+                      </div>
+                    </Popover.Panel>
+                  </Transition>
+                </>
+              )}
+            </Popover>
+          </li>
+        </ul>
+      </div>
+    </Popover>
+  );
+};
+
+export default Navbar;
